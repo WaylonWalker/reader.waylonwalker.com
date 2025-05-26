0f65efad598b436ca743ebd3f835647e04e08c49
Waylon S. Walker <waylon@waylonwalker>
2025-05-26 11:35:53 -0500

    add blogroll

--- reader_waylonwalker_com/plugins/reader.py
+++ reader_waylonwalker_com/plugins/reader.py
@@ -10,6 +10,7 @@ import pydantic
 
 from markata.hookspec import hook_impl, register_attr
 from markata import background
+from markata.plugins.feeds import FeedConfig
 
 
 class Feed(pydantic.BaseModel):
@@ -64,13 +65,55 @@ def load(markata) -> None:
 
     markata.console.log("Loaded articles")
 
+    markata.config.feeds.append(
+        FeedConfig(
+            title="Blogroll",
+            slug="blogroll",
+            description="Blogroll - a collection of awesome people I follow online",
+            name="blogroll",
+            filter="'blogroll' in tags",
+        )
+    )
+
     if "articles" not in markata.__dict__:
         markata.articles = []
     for feed in markata.config.reader:
         markata.console.log(f"Creating posts for: {feed.url}")
         markata.console.log(f"feed has {len(feed.feed['entries'])} entries")
+        markata.config.feeds.append(
+            FeedConfig(
+                title=f"{feed.author}'s posts",
+                slug=f"{feed.author.replace(' ', '-').lower()}-posts",
+                description=f"{feed.author}'s posts from {feed.url}",
+                name=f"{feed.author.replace(' ', '_').lower()}_posts",
+                filter=f"'{feed.author.replace(' ', '_').lower()}' in tags",
+                sort="date",
+                reverse=True,
+            )
+        )
 
-        for post in feed.feed["entries"][0 : feed.limit]:
+        from urllib.parse import urlparse
+
+        base_url = urlparse(feed.url).scheme + "://" + urlparse(feed.url).netloc
+        image = f"https://shots.wayl.one/shot/?url={base_url}&height=600&width=1200&scaled_width=600&scaled_height=300"
+
+        article = markata.Post(
+            markata=markata,
+            path="None",
+            date=datetime.datetime.now(),
+            datetime=datetime.datetime.now(),
+            title=feed.author,
+            content=f"{feed.author}'s posts from {feed.url}",
+            slug=feed.author.replace(" ", "-").lower(),
+            link=f"/{feed.author.replace(' ', '-').lower()}-posts",
+            image=image,
+            tags=["blogroll"],
+            skip=False,
+        )
+
+        markata.articles.append(article)
+
+        for i, post in enumerate(feed.feed["entries"]):
             if post.get("title") == "":
                 post["title"] = post.get("link")
             if "<" in post.get("summary", ""):
@@ -99,6 +142,11 @@ def load(markata) -> None:
                 raw_date = None
                 markata.console.log("No date found in feed entry")
 
+            if i <= feed.limit:
+                tags = "main"
+            else:
+                tags = "hidden"
+
             article = markata.Post(
                 markata=markata,
                 path="None",
@@ -112,7 +160,7 @@ def load(markata) -> None:
                 slug=post.get("link"),
                 link=post.get("link"),
                 image=image,
-                tags=feed.tags,
+                tags=[*feed.tags, feed.author.replace(" ", "_").lower(), tags],
                 skip=False,
             )
             markata.articles.append(article)
